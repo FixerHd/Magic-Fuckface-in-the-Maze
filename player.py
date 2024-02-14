5157cd29f226171b3b865ea5af949bd0e646d924
FixerHd <fixerhd03@gmail.com>
2024-02-14 13:24:38 +0100

    collision

--- player.py
+++ player.py
@@ -6,12 +6,13 @@ import math
 class Player:
     def __init__(self, game):
         self.game = game
-        self.x, self.y = 2, 2
+        self.x, self.y = PLAYER_POS
         self.angle = PLAYER_ANGLE
 
     def movement(self):
         sin_a = math.sin(self.angle)
         cos_a = math.cos(self.angle)
+        dx, dy = 0, 0
         speed = PLAYER_SPEED * self.game.delta_time
         speed_sin = speed * sin_a
         speed_cos = speed * cos_a
@@ -19,26 +20,35 @@ class Player:
         keys = pg.key.get_pressed()
 
         if keys[pg.K_w]:
-            self.x += speed_cos
-            self.y += speed_sin
+            dx += speed_cos
+            dy += speed_sin
         if keys[pg.K_s]:
-            self.x += -speed_cos
-            self.y += -speed_sin
+            dx += -speed_cos
+            dy += -speed_sin
         if keys[pg.K_a]:
-            self.x += speed_cos
-            self.y += -speed_sin
+            dx += speed_cos
+            dy += -speed_sin
         if keys[pg.K_d]:
-            self.x += -speed_cos
-            self.y += speed_sin
+            dx += -speed_cos
+            dy += speed_sin
 
-        self.x = self.x
-        self.y = self.y
+        self.check_wall_collision(dx, dy)
+
 
         if keys[pg.K_LEFT]:
             self.angle -= PLAYER_ROTSPEED * self.game.delta_time
         if keys[pg.K_RIGHT]:
             self.angle += PLAYER_ROTSPEED * self.game.delta_time
         self.angle %= math.tau
+
+    def check_wall(self, x, y):
+        return (x, y) not in self.game.map.world_map
+
+    def check_wall_collision(self, dx, dy):
+        if self.check_wall(int(self.x + dx), int(self.y)):
+            self.x += dx
+        if self.check_wall(int(self.x), int(self.y + dy)):
+            self.y += dy
 
     def draw(self):
         pg.draw.line(self.game.screen, 'green', (self.x * 55, self.y * 37),
