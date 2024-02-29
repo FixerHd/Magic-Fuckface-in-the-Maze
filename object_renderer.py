59b493ee795a4492dc192da4811c4965f5954e20
FixerHD <fixerhd03@gmail.com>
2024-02-29 13:07:52 +0100

    cosas

--- object_renderer.py
+++ object_renderer.py
@@ -9,11 +9,6 @@ class ObjectRenderer:
         self.wall_textures = self.load_wall_textures()
         self.sky_image = self.get_texture('resources/textures/sky.png', (WIDTH, HALF_HEIGHT))
         self.sky_offset = 0
-        self.blood_screen = self.get_texture('resources/textures/blood_screen.png', RES)
-        self.digit_size = 90
-        self.digit_images = [self.get_texture(f'resources/textures/digits/{i}.png', [self.digit_size] * 2)
-                             for i in range(11)]
-        self.digits = dict(zip(map(str, range(11)), self.digit_images))
         self.game_over_image = self.get_texture('resources/textures/game_over.png', RES)
         self.win_image = self.get_texture('resources/textures/win.png', RES)
 
@@ -30,12 +25,10 @@ class ObjectRenderer:
 
     def draw_player_health(self):
         health = str(self.game.player.health)
-        for i, char in enumerate(health):
-            self.screen.blit(self.digits[char], (i * self.digit_size, 0))
-        self.screen.blit(self.digits['10'], ((i + 1) * self.digit_size, 0))
+        font = pg.font.Font(None, 100)
+        health_text = font.render("Health: " + health, True, (0, 255, 0))
+        self.screen.blit(health_text, (0, 0))
 
-    def player_damage(self):
-        self.screen.blit(self.blood_screen, (0, 0))
 
     def draw_background(self):
         self.sky_offset = (self.sky_offset + 4.5 * self.game.player.rel) % WIDTH
