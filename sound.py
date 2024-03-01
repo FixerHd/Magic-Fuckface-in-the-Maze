843df8e79a5d435dd936415d6a0b9e4eae1c8452
FixerHD <fixerhd03@gmail.com>
2024-03-01 11:12:32 +0100

    fart

--- sound.py
+++ sound.py
@@ -16,7 +16,7 @@ class Sound:
         self.npc_pain.set_volume(0.3)
         self.player_pain = pg.mixer.Sound(self.path + 'player_pain.mp3')
         self.theme = pg.mixer.music.load(self.path + 'theme.mp3')
-        pg.mixer.music.set_volume(1)
+        pg.mixer.music.set_volume(0.5)
 
     def stop_music(self):
         pg.mixer.music.set_volume(0)
